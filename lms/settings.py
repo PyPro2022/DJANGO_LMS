f612e29aec279d8517e3ce545f751c543baf98a9
PyPro2022 <pyprohillel2022@protonmail.com>
2022-06-03 15:57:49 +0300

    Поменял формат даты для студентов

--- lms/settings.py
+++ lms/settings.py
@@ -143,3 +143,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+USE_L10N = False
+DATE_FORMAT = 'd. m. Y'
