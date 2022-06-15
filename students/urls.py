10e077dc43d7178108e56dcb83502933e61386c8
PyPro2022 <pyprohillel2022@protonmail.com>
2022-06-15 19:29:32 +0300

    Добавил list, create, delete CBV студентам. Прописал маршруты, поправил list-шаблон

--- students/urls.py
+++ students/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from .views import * # noqa
 
 urlpatterns = [
-    path('', get_students, name='students'),  # noqa
-    path('create/', create_student, name='create_student'),  # noqa
+    # path('', get_students, name='students'),  # noqa
+    path('', ListStudentView.as_view(), name='students'),  # noqa
+    # path('create/', create_student, name='create_student'),  # noqa
+    path('create/', CreateStudentView.as_view(), name='create_student'),  # noqa
+
     # path('update/<int:pk>/', update_student, name='update_student'),  # noqa
     path('update/<int:identity>/', UpdateStudentView.as_view(), name='update_student'),  # noqa
-    path('delete/<int:pk>/', delete_student, name='delete_student'),  # noqa
-    path('delete/<int:pk>/', delete_student, name='delete_student'),  # noqa
-    path('generate_students/', generate_students, name='gen_students'),  # noqa
+    path('delete/<int:identity>/', DeleteStudentView.as_view(), name='delete_student'),  # noqa
+    # path('delete/<int:pk>/', delete_student, name='delete_student'),  # noqa
+    # path('generate_students/', generate_students, name='gen_students'),  # noqa
 ]
--- students/views.py
+++ students/views.py
@@ -1,89 +1,102 @@
 # .../DJANGO_LMS/students/views.py
 
-__all__ = ['generate_students',
-           'create_student',
-           'get_students',
-           # 'update_student',
-           'delete_student',
-           'UpdateStudentView',
+__all__ = [
+            'UpdateStudentView',
+            'ListStudentView',
+            'CreateStudentView',
+            'DeleteStudentView',
            ]
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import UpdateView
 
-from webargs.djangoparser import use_kwargs  # ,use_args
-from webargs.fields import Int  # ,Str, Date
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 from .forms import StudentCreateForm, StudentFilterForm, StudentUpdateForm
 from .models import Student
 
 
-@use_kwargs(
-    {'cnt': Int(required=False)},
-    location='query'
-)
-def generate_students(request, cnt=0, max_number=100):
-    if cnt == 10:
-        Student.gen_students(cnt=0)
-        return HttpResponseRedirect(reverse('students'))
-    else:
-        Student.gen_students(cnt=cnt)
-        if cnt:
-            return HttpResponseRedirect(reverse('students'))
-    return render(
-        request,
-        'students/generate_students.html',
-        {'title': 'Generate students',
-         'message01': f'Input the number of students (max = {max_number}) you wish to generate.',
-         # 'message02': f'{cnt} teacher(-s) has been generated',
-         'max': max_number}  # , 'kwargs': kwargs}
-    )
+class ListStudentView(ListView):
+    model = Student
+    template_name = 'students/st_list.html'
+    context_object_name = 'students_filter'
+
+    def get_queryset(self):
+        students_filter = StudentFilterForm(
+            data=self.request.GET,
+            queryset=self.model.objects.all().select_related('group', 'headman_group')
+        )
+
+        return students_filter
 
 
-def get_students(request):
-    students = Student.objects.all().select_related('group', 'headman_group')
-    students_filter = StudentFilterForm(data=request.GET, queryset=students)
-    return render(
-        request,
-        'students/st_list.html',
-        {'students_filter': students_filter}  # , 'students': students}
-    )
-
-
-def create_student(request):
-    if request.method == 'GET':
-        form = StudentCreateForm()
-    else:
-        form = StudentCreateForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('students'))
-    return render(
-        request,
-        'students/st_create.html',
-        {'title': 'Create student', 'form': form}
-    )
+class CreateStudentView(CreateView):
+    model = Student
+    form_class = StudentCreateForm
+    success_url = reverse_lazy('students')
+    template_name = 'students/st_create.html'
+    extra_context = {'title': 'Create student'}
 
 
 class UpdateStudentView(UpdateView):
     pk_url_kwarg = 'identity'
     model = Student
-    form_class = StudentCreateForm
+    form_class = StudentUpdateForm
     success_url = reverse_lazy('students')
     template_name = 'students/st_update.html'
-    # context = {'title': 'Update student', 'form': form, 'student':student}
 
 
-def delete_student(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    if request.method == 'POST':
-        student.delete()
-        return HttpResponseRedirect(reverse('students'))
-    return render(request, 'students/st_delete.html', {'student': student})
+class DeleteStudentView(DeleteView):
+    pk_url_kwarg = 'identity'
+    model = Student
+    success_url = reverse_lazy('students')
+    template_name = 'students/st_delete.html'
+
+
+
+
 
 # Кладовка
+
+## Импорты
+
+# __all__ = [#'generate_students',
+           # 'create_student',
+           # 'get_students',
+           # 'update_student',
+           # 'delete_student',
+    # ]
+
+# from webargs.djangoparser import use_kwargs  # ,use_args
+# from webargs.fields import Int  # ,Str, Date
+
+##Вьюхи
+
+# def get_students(request):
+#     students = Student.objects.all().select_related('group', 'headman_group')
+#     students_filter = StudentFilterForm(data=request.GET, queryset=students)
+#     return render(
+#         request,
+#         'students/st_list.html',
+#         {'students_filter': students_filter}  # , 'students': students}
+#     )
+#
+
+# def create_student(request):
+#     if request.method == 'GET':
+#         form = StudentCreateForm()
+#     else:
+#         form = StudentCreateForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('students'))
+#     return render(
+#         request,
+#         'students/st_create.html',
+#         {'title': 'Create student', 'form': form}
+#     )
+
 
 # def update_student(request, pk):
 #     student = get_object_or_404(Student, pk=pk)
@@ -99,16 +112,36 @@ def delete_student(request, pk):
 #         {'title': 'Update student', 'form': form, 'student':student},
 #     )
 
+# def delete_student(request, pk):
+#     student = get_object_or_404(Student, pk=pk)
+#     if request.method == 'POST':
+#         student.delete()
+#         return HttpResponseRedirect(reverse('students'))
+#     return render(request, 'students/st_delete.html', {'student': student})
 
 
 
-
-
-
-# попытка реализовать вывод содержимого _README.md на главную страницу LMS. Пусть пока полежит. #noqa
-# def readme_md(request):                                                                      #noqa
-#     return render(request, 'README.html')                                                    #noqa
-
+# @use_kwargs(
+#     {'cnt': Int(required=False)},
+#     location='query'
+# )
+# def generate_students(request, cnt=0, max_number=100):
+#     if cnt == 10:
+#         Student.gen_students(cnt=0)
+#         return HttpResponseRedirect(reverse('students'))
+#     else:
+#         Student.gen_students(cnt=cnt)
+#         if cnt:
+#             return HttpResponseRedirect(reverse('students'))
+#     return render(
+#         request,
+#         'students/generate_students.html',
+#         {'title': 'Generate students',
+#          'message01': f'Input the number of students (max = {max_number}) you wish to generate.',
+#          # 'message02': f'{cnt} teacher(-s) has been generated',
+#          'max': max_number}  # , 'kwargs': kwargs}
+#     )
+#
 
 # @use_args(
 #     {
