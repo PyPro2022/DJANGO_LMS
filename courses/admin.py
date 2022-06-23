c3d594402dec7b0a03e5ce4ca39eef901098e6a2
PyPro2022 <pyprohillel2022@protonmail.com>
2022-06-23 21:32:37 +0300

    Добавил курсы в админку

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course
+
+# from groups.admin import GroupAdmin
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'duration',
+        'price',
+            ]
+
+    fields = [
+        ('name', 'duration', 'price'),
+        ('create_datetime', 'update_datetime')
+    ]
+
+    readonly_fields = ['create_datetime', 'update_datetime']
+
+    # inlines = [GroupAdmin]
+
+    # def get_form(self, request, obj=None, **kwargs):
+    #     form = super().get_form(request, obj, **kwargs)
+    #     form.base_fields['headman'].widget.can_add_related = False
+    #     return form
+
+
+admin.site.register(Course, CourseAdmin)
+
+
+# Кладовка
